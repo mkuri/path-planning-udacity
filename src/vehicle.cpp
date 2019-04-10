2ef551cd9ecdc855305dafe548b60f469266ec2a
Makoto Kurihara <mkuri8m@gmail.com>
2019-04-11 00:53:06 +0900

    feat: Add Finite state machine and cost functions

--- src/vehicle.cpp
+++ src/vehicle.cpp
@@ -1,6 +1,8 @@
 #include "vehicle.h"
 
-Vehicle::Vehicle() {};
+Vehicle::Vehicle() {
+  this->target_lane = 1;
+};
 
 int Vehicle::get_lane(double d) {
   int lane = d / 4;
@@ -31,25 +33,235 @@ void Vehicle::print() {
 void Vehicle::next_state(nlohmann::basic_json<>& sensor_fusion) {
   this->target_vel = SPEED_LIMIT;
   this->target_acc = ACC_LIMIT;
+  this->state = State::KL;
+  this->closest_vehicle_ids = interpret_sensor_fusion(sensor_fusion);
+
+  if (this->lane == 0) {
+    std::cout << "im in the lane 0" << std::endl;
+    int front_vehicle_id = closest_vehicle_ids[0][0];
+    double cost_kl = cost_lane(0, sensor_fusion);
+    double cost_lcr = cost_lane(1, sensor_fusion);
+    std::cout << "cost_kl: " << cost_kl << std::endl;
+    std::cout << "cost_lcr: " << cost_lcr << std::endl;
+    if (cost_kl <= cost_lcr) {
+      this->target_lane = this->lane;
+      this->state = State::KL;
+      if (front_vehicle_id >= 0) {
+        double s = sensor_fusion[front_vehicle_id][5];
+        double vx = sensor_fusion[front_vehicle_id][3];
+        double vy = sensor_fusion[front_vehicle_id][4];
+        double v = sqrt(vx*vx + vy*vy);
+        if((s - this->s) < 30) {
+          this->target_vel = v;
+        }
+      }
+    } else {
+      this->target_lane = 1;
+      this->state = State::LCR;
+    }
+  } else if (this->lane == 1) {
+    std::cout << "im in the lane 1" << std::endl;
+    int front_vehicle_id = closest_vehicle_ids[1][0];
+    double cost_kl = cost_lane(1, sensor_fusion);
+    double cost_lcl = cost_lane(0, sensor_fusion);
+    double cost_lcr = cost_lane(2, sensor_fusion);
+    std::cout << "cost_kl: " << cost_kl << std::endl;
+    std::cout << "cost_lcl: " << cost_lcl << std::endl;
+    std::cout << "cost_lcr: " << cost_lcr << std::endl;
+
+    if (cost_kl <= cost_lcl && cost_kl <= cost_lcr) {
+      this->target_lane = this->lane;
+      this->state = State::KL;
+      if (front_vehicle_id >= 0) {
+        double s = sensor_fusion[front_vehicle_id][5];
+        double vx = sensor_fusion[front_vehicle_id][3];
+        double vy = sensor_fusion[front_vehicle_id][4];
+        double v = sqrt(vx*vx + vy*vy);
+        if((s - this->s) < 30) {
+          this->target_vel = v;
+        }
+      }
+    } else if (cost_lcl <= cost_lcr) {
+      this->target_lane = 0;
+      this->state = State::LCL;
+    } else {
+      this->target_lane = 2;
+      this->state = State::LCR;
+    }
+  } else if (this->lane == 2) {
+    std::cout << "im in the lane 2" << std::endl;
+    int front_vehicle_id = closest_vehicle_ids[2][0];
+    double cost_kl = cost_lane(2, sensor_fusion);
+    double cost_lcl = cost_lane(1, sensor_fusion);
+    std::cout << "cost_kl: " << cost_kl << std::endl;
+    std::cout << "cost_lcl: " << cost_lcl << std::endl;
+    if (cost_kl <= cost_lcl) {
+      this->target_lane = this->lane;
+      this->state = State::KL;
+      if (front_vehicle_id >= 0) {
+        double s = sensor_fusion[front_vehicle_id][5];
+        double vx = sensor_fusion[front_vehicle_id][3];
+        double vy = sensor_fusion[front_vehicle_id][4];
+        double v = sqrt(vx*vx + vy*vy);
+        if((s - this->s) < 30) {
+          this->target_vel = v;
+        }
+      }
+    } else {
+      this->target_lane = 1;
+      this->state = State::LCL;
+    }
+  }
+}
+
+std::vector<std::vector<int>> Vehicle::interpret_sensor_fusion(nlohmann::basic_json<>& sensor_fusion) {
+  std::vector<std::vector<int>> ids(NUM_LANE, std::vector<int>(2, -1));
+  std::vector<std::vector<double>> distances(NUM_LANE, std::vector<double>(2, 100000));
 
   for (int i = 0; i < sensor_fusion.size(); ++i) {
-    double id = sensor_fusion[i][0];
     double x = sensor_fusion[i][1];
     double y = sensor_fusion[i][2];
-    double vx = sensor_fusion[i][3];
-    double vy = sensor_fusion[i][4];
     double s = sensor_fusion[i][5];
     double d = sensor_fusion[i][6];
     double lane = Vehicle::get_lane(d);
-    double v = sqrt(vx*vx + vy*vy);
-    
-    if (lane == this->lane) {
-      if (s > this->s && (s - this->s) < 30) {
-        std::cout << "Other vehicle is near. The vehicle run at " << v << " m/s" << std::endl;
-        this->target_vel = v;
+
+    double dist = distance(this->x, this->y, x, y);
+    std::cout << "lane: " << lane << ", dist: " << dist << std::endl;
+    if (0 <= lane && lane <= NUM_LANE-1) {
+      if (s > this->s) {
+        if (dist < distances[lane][0]) {
+          std::cout << "a" << std::endl;
+          distances[lane][0] = dist;
+          ids[lane][0] = i;
+        }
+      } else {
+        if (dist < distances[lane][1]) {
+          std::cout << "b" << std::endl;
+          distances[lane][1] = dist;
+          ids[lane][1] = i;
+        }
       }
     }
   }
+  return ids;
+}
+
+double Vehicle::cost_lane(int lane, nlohmann::basic_json<>& sensor_fusion) {
+  double cost = 0;
+  int fr_id = this->closest_vehicle_ids[lane][0];
+  int bk_id = this->closest_vehicle_ids[lane][1];
+  double fr_dist = 100000;
+  double fr_v = 0;
+  bool is_fr_vehicle = false;
+  double bk_dist = 100000;
+  double bk_v = 0;
+  bool is_bk_vehicle = false;
+
+  if (fr_id >= 0) {
+    double fr_s = sensor_fusion[fr_id][5];
+    fr_dist = fr_s - this->s;
+    double fr_vx = sensor_fusion[fr_id][3];
+    double fr_vy = sensor_fusion[fr_id][4];
+    fr_v = sqrt(fr_vx*fr_vx + fr_vy*fr_vy);
+    is_fr_vehicle = true;
+  }
+  if (bk_id >= 0) {
+    double bk_s = sensor_fusion[bk_id][5];
+    bk_dist = this->s - bk_s;
+    double bk_vx = sensor_fusion[bk_id][3];
+    double bk_vy = sensor_fusion[bk_id][4];
+    bk_v = sqrt(bk_vx*bk_vx + bk_vy*bk_vy);
+    is_bk_vehicle = true;
+  }
+
+  // double a = fr_dist + fr_v * (fr_dist / SPEED_LIMIT - fr_v);
+  double a = fr_dist;
+  cost += exp(1/a);
+
+  // Check collision
+  if (bk_dist < 10 && bk_v > this->vel) {
+    cost += 10;
+  }
+
+  return cost;
+}
+
+double Vehicle::cost_keep_lane(nlohmann::basic_json<>& sensor_fusion) {
+  double cost = 1;
+
+  int fr_id = this->closest_vehicle_ids[this->lane][0];
+  int bk_id = this->closest_vehicle_ids[this->lane][1];
+  double fr_dist = 100000;
+  double fr_v = 0;
+  bool is_fr_vehicle = false;
+  double bk_dist = 100000;
+  double bk_v = 0;
+  bool is_bk_vehicle = false;
+
+  if (fr_id >= 0) {
+    double fr_s = sensor_fusion[fr_id][5];
+    fr_dist = fr_s - this->s;
+    double fr_vx = sensor_fusion[fr_id][3];
+    double fr_vy = sensor_fusion[fr_id][4];
+    fr_v = sqrt(fr_vx*fr_vx + fr_vy*fr_vy);
+    is_fr_vehicle = true;
+  }
+  if (bk_id >= 0) {
+    double bk_s = sensor_fusion[bk_id][5];
+    bk_dist = this->s - bk_s;
+    double bk_vx = sensor_fusion[bk_id][3];
+    double bk_vy = sensor_fusion[bk_id][4];
+    bk_v = sqrt(bk_vx*bk_vx + bk_vy*bk_vy);
+    is_bk_vehicle = true;
+  }
+
+  // Check free space
+  if (fr_dist > 100) {
+    cost -= 10;
+  }
+  
+  return cost;
+}
+
+double Vehicle::cost_lane_change(int lane, nlohmann::basic_json<>& sensor_fusion) {
+  double cost = 0;
+
+  int fr_id = this->closest_vehicle_ids[lane][0];
+  int bk_id = this->closest_vehicle_ids[lane][1];
+  double fr_dist = 100000;
+  double fr_v = 0;
+  bool is_fr_vehicle = false;
+  double bk_dist = 100000;
+  double bk_v = 0;
+  bool is_bk_vehicle = false;
+
+  if (fr_id >= 0) {
+    double fr_s = sensor_fusion[fr_id][5];
+    fr_dist = fr_s - this->s;
+    double fr_vx = sensor_fusion[fr_id][3];
+    double fr_vy = sensor_fusion[fr_id][4];
+    fr_v = sqrt(fr_vx*fr_vx + fr_vy*fr_vy);
+    is_fr_vehicle = true;
+  }
+  if (bk_id >= 0) {
+    double bk_s = sensor_fusion[bk_id][5];
+    bk_dist = this->s - bk_s;
+    double bk_vx = sensor_fusion[bk_id][3];
+    double bk_vy = sensor_fusion[bk_id][4];
+    bk_v = sqrt(bk_vx*bk_vx + bk_vy*bk_vy);
+    is_bk_vehicle = true;
+  }
+
+  // Check collision
+  if (fr_dist < 15 || bk_dist < 10) {
+    cost += 5;
+  }
+  // Check free space
+  if (fr_dist > 100 && bk_dist > 30) {
+    cost -= 5;
+  }
+
+  return cost;
 }
 
 std::tuple<std::vector<double>, std::vector<double>> Vehicle::get_tragectory(
@@ -97,21 +309,22 @@ std::tuple<std::vector<double>, std::vector<double>> Vehicle::get_tragectory(
     next_y_vals.push_back(previous_path_y[i]);
   }
 
-  int target_lane = 1;
   // Generate target xy points
-  std::vector<double> wp0 = getXY(this->s+30, 2+4*target_lane,
-      map_waypoints_s, map_waypoints_x, map_waypoints_y);
-  std::vector<double> wp1 = getXY(this->s+60, 2+4*target_lane,
-      map_waypoints_s, map_waypoints_x, map_waypoints_y);
-  std::vector<double> wp2 = getXY(this->s+90, 2+4*target_lane,
-      map_waypoints_s, map_waypoints_x, map_waypoints_y);
+  std::vector<std::vector<double>> wps;
+  if (this->state == State::KL) {
+    wps = Vehicle::get_wps_keep_lane(map_waypoints_x, map_waypoints_y, map_waypoints_s);
+  } else if(this->state == State::LCL) {
+    wps = Vehicle::get_wps_lane_change(this->target_lane,
+        map_waypoints_x, map_waypoints_y, map_waypoints_s);
+  } else if(this->state == State::LCR) {
+    wps = Vehicle::get_wps_lane_change(this->target_lane,
+        map_waypoints_x, map_waypoints_y, map_waypoints_s);
+  }
 
-  target_xs.push_back(wp0[0]);
-  target_xs.push_back(wp1[0]);
-  target_xs.push_back(wp2[0]);
-  target_ys.push_back(wp0[1]);
-  target_ys.push_back(wp1[1]);
-  target_ys.push_back(wp2[1]);
+  for(auto wp: wps) {
+    target_xs.push_back(wp[0]);
+    target_ys.push_back(wp[1]);
+  }
 
   std::cout << "target_xs: " << std::endl;
   for(auto tx: target_xs) {
@@ -202,4 +415,41 @@ std::tuple<std::vector<double>, std::vector<double>> Vehicle::get_tragectory(
 
   auto trajectory = std::make_tuple(next_x_vals, next_y_vals);
   return trajectory;
+}
+
+std::vector<std::vector<double>> Vehicle::get_wps_keep_lane(
+    std::vector<double>& map_waypoints_x,
+    std::vector<double>& map_waypoints_y,
+    std::vector<double>& map_waypoints_s) {
+  std::vector<std::vector<double>> wps;
+  std::vector<double> wp0 = getXY(this->s + 30, 2+4*this->target_lane,
+      map_waypoints_s, map_waypoints_x, map_waypoints_y);
+  wps.push_back(wp0);
+  std::vector<double> wp1 = getXY(this->s + 60, 2+4*this->target_lane,
+      map_waypoints_s, map_waypoints_x, map_waypoints_y);
+  wps.push_back(wp1);
+  std::vector<double> wp2 = getXY(this->s + 90, 2+4*this->target_lane,
+      map_waypoints_s, map_waypoints_x, map_waypoints_y);
+  wps.push_back(wp2);
+
+  return wps;
+}
+
+std::vector<std::vector<double>> Vehicle::get_wps_lane_change(
+    int target_lane,
+    std::vector<double>& map_waypoints_x,
+    std::vector<double>& map_waypoints_y,
+    std::vector<double>& map_waypoints_s) {
+  std::vector<std::vector<double>> wps;
+  std::vector<double> wp0 = getXY(this->s + 60, 2+4*target_lane,
+      map_waypoints_s, map_waypoints_x, map_waypoints_y);
+  wps.push_back(wp0);
+  std::vector<double> wp1 = getXY(this->s + 90, 2+4*target_lane,
+      map_waypoints_s, map_waypoints_x, map_waypoints_y);
+  wps.push_back(wp1);
+  std::vector<double> wp2 = getXY(this->s + 120, 2+4*target_lane,
+      map_waypoints_s, map_waypoints_x, map_waypoints_y);
+  wps.push_back(wp2);
+
+  return wps;
 }
--- src/vehicle.h
+++ src/vehicle.h
@@ -13,6 +13,7 @@ const double ACC_LIMIT = 9.0; // m/s^2  margin = 1.0m/s^2
 const double JERK_LIMIT = 9.0; // m/s^3  margin = 1.0m/s^3
 const int NUM_TRAJECTORY = 50;
 const double DT = 0.02; // Delta time
+const int NUM_LANE = 3;
 
 enum class State { KL, PLCL, LCL, PLCR, LCR };
 
@@ -44,8 +45,25 @@ class Vehicle {
     double target_acc;
     double order_acc;
     State state;
+    std::vector<std::vector<int>> closest_vehicle_ids;
 
     int lane;
+    int target_lane;
+
+    std::vector<std::vector<int>> interpret_sensor_fusion(nlohmann::basic_json<>& sensor_fusion);
+    double cost_keep_lane(nlohmann::basic_json<>& sensor_fusion);
+    double cost_lane_change(int lane, nlohmann::basic_json<>& sensor_fusion);
+    double cost_lane(int lane, nlohmann::basic_json<>& sensor_fusion);
+    std::vector<std::vector<double>> get_wps_keep_lane(
+        std::vector<double>& map_waypoints_x,
+        std::vector<double>& map_waypoints_y,
+        std::vector<double>& map_waypoints_s);
+    std::vector<std::vector<double>> get_wps_lane_change(
+        int target_lane,
+        std::vector<double>& map_waypoints_x,
+        std::vector<double>& map_waypoints_y,
+        std::vector<double>& map_waypoints_s);
+
 };
 
 #endif
